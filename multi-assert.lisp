ee924637e11943a504e7f42e996cf7b8ec58b11c
Michał Herda <phoe@openmailbox.org>
2016-03-10 16:11:07 +0100

    Create multi-assert.lisp

---
+++ multi-assert.lisp
@@ -0,0 +1,21 @@
+;; MULTI-ASSERT by Michal Herda
+;; License: GNU GPLv3
+
+(defmacro multi-assert (vars predicates)
+  (if predicates
+      `(assert (progn (multi-assert ,vars ,(cdr predicates))
+		      ,(caar predicates))
+	       ,vars ,(cadar predicates) ,(cddadr predicates))))
+
+;; Example usage, customize the X value to your liking.
+
+(let ((x 15))
+  (multi-assert (x)
+    (((typep x 'integer)
+      "X must be an integer.")
+     ((/= x 25)
+      "X must not equal 25.") 
+     ((< x 50)
+      "X must be less than 50.")
+     ((> x 0)
+      "X must be more than 0."))))
